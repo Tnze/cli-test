5d469cbe148d77c4aab175c699c8c626de63253d
Tnze <cjd001113@outlook.com>
2023-12-18 19:27:10 +0800

    Upload latest code used in the proof video

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::Path;
+
+use serde_json::Value;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,6 +13,14 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let args = app.get_cli_matches()?.args;
+            if let Value::String(file) = &args.get("file").unwrap().value {
+                let file = Path::new(&file);
+                println!("{file:?}");
+            }
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
